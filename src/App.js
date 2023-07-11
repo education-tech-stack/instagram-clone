9fd728c985d21284d38099de2f0ddbb678d6126c
Sameer Chauhan <sameer55chauhan@gmail.com>
2023-07-12 02:55:21 +0530

    added footer

--- src/App.js
+++ src/App.js
@@ -19,6 +19,14 @@ export default function App() {
   return (
     <UserContext.Provider value={{ user }}>
       <Router>
+        <footer className="fixed bottom-0 left-0 z-20 w-full p-4 border-t shadow md:flex md:items-center md:justify-between md:p-6 bg-gray-800 border-gray-600">
+          <span className="text-sm sm:text-center text-gray-400">
+            IMPORTANT: This is a clone Project created by{' '}
+            <a href="https://github.com/sameer55chauhan">Sameer Chauhan</a>. No data is being
+            collected by this website, other than the SignUp Details. Recommended: to fill the fake
+            SignUp details if you want to go through it.
+          </span>
+        </footer>
         <Suspense fallback={<ReactLoader />}>
           <Switch>
             <Route path={ROUTES.LOGIN} component={Login} />
